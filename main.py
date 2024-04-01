25f45031279aa31807ecc6b84687b92a9edda02a
Fragly <hendrik@isdev.co>
2024-04-01 21:26:57 +0200

    aasadsasdsadasdasd

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 import os
 
+# adding this comment cuz it didn't detect my previous commit
 app = Flask(__name__)
 model_path = os.environ.get('AI_MODEL_PATH')
 classifier = pipeline("zero-shot-classification", model=(f'{dirname(__file__)}{model_path}'))
