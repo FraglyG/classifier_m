ee0731103284e28e9c67c8d5a68aa61397303bd9
Fragly <hendrik@isdev.co>
2024-04-01 19:53:16 +0200

    no cuda compatibility update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
-from transformers import pipeline
+import torch
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 import os
 
 app = Flask(__name__)
 model_path = os.environ.get('AI_MODEL_PATH')
-classifier = pipeline("zero-shot-classification", model=model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path, from_tf=False)
+
+classifier = pipeline("zero-shot-classification", model=model, tokenizer=tokenizer, device=0 if torch.cuda.is_available() else -1)
 
 @app.route('/classify', methods=['POST'])
 def classify():
