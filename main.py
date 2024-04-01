02f3d4807ba742e76b0472e8deacf5edf3026ad1
Fragly <hendrik@isdev.co>
2024-04-01 18:51:03 +0200

    Created the classifier service
    The classifier_m service is a zero-shot classification service
    service is meant to aid alfred in classifying user input to better assist users

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+from flask import Flask, request, jsonify
+from transformers import pipeline
+import os
+
+app = Flask(__name__)
+model_path = os.environ.get('AI_MODEL_PATH')
+classifier = pipeline("zero-shot-classification", model=model_path)
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    data = request.get_json()
+    sequence_to_classify = data['sequence']
+    candidate_labels = data['labels']
+    output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
